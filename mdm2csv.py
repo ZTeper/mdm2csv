a4fb236eba085698a374ae2f5fd430cf46f20467
Zachary Teper <zachary.teper@gmail.com>
2024-03-16 07:10:11 -0400

    Create a new python script

---
+++ mdm2csv.py
@@ -0,0 +1,20 @@
+
+# Zachary Teper
+# March 16, 2024
+
+import csv
+
+inputFileName = input("MDM file name (*.mdm file extension): ")
+
+print("Parsing input file...")
+# Parse input file
+print("Done parsing input")
+
+outputFileName = input("CSV file name (*.csv file extension): ")
+
+print("Writing output file...")
+# Write output file
+print("Done writing output")
+
+print("done")
+
